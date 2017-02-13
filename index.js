cfae21e94c099d42bf12ac188edac7486aacf2e7
pavex <pavex@ines.cz>
2017-02-13 20:25:20 +0100

    General update

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 import Application from './lib/Application.js';
-import Component from './lib/Component.js';
 import Scene from './lib/Scene.js';
 //
 import Alert from './lib/scenes/Alert.js';
 import Confirm from './lib/scenes/Confirm.js';
 import Error from './lib/scenes/Error.js';
 //
-import Dialog from './lib/ui/Dialog.js';
-import Layout from './lib/ui/Layout.js';
-import Toolbar from './lib/ui/Toolbar.js';
-import Input from './lib/ui/Input.js';
-import Form from './lib/ui/Form.js';
-import Loader from './lib/ui/Loader.js';
-import Block from './lib/ui/Block.js';
-//import Menu from './lib/ui/Menu/Menu.js';
+import Dialog from './lib/ui/Dialog/';
+import Layout from './lib/ui/Layout/';
+import Toolbar from './lib/ui/Toolbar/';
+import Input from './lib/ui/Input/';
+import Form from './lib/ui/Form/';
+import Loader from './lib/ui/Loader/';
 import Menu from './lib/ui/Menu/';
-import DataGrid from './lib/ui/DataGrid.js';
+import DataGrid from './lib/ui/DataGrid/';
 
 
 /**
@@ -24,9 +21,6 @@ module.exports = {
 
 // Application controller
 	Application: Application,
-
-// Extended React-JS UI component
-	Component: Component,
 
 // Exteded component/Scene component with Navigator
 	Scene: Scene,
@@ -42,8 +36,9 @@ module.exports = {
 	Toolbar: Toolbar,
 	Input: Input,
 	Form: Form,
-	Block: Block,
 	Loader: Loader,
 	Menu: Menu,
+
+// Advanced UI
 	DataGrid: DataGrid
 };
